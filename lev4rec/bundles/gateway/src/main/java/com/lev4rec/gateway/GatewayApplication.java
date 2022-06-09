362bcfa79aca24eeab8ad57161374c2fdbd00f55
recsys_2021 <conference834@gmail.com>
2022-06-09 10:12:27 +0200

    Redirect urls have been fixed

--- lev4rec/bundles/gateway/src/main/java/com/lev4rec/gateway/GatewayApplication.java
+++ lev4rec/bundles/gateway/src/main/java/com/lev4rec/gateway/GatewayApplication.java
@@ -18,118 +18,118 @@ public class GatewayApplication {
 		 return builder.routes()
 			        .route(p-> p.path("/lev4rec/").and().method("GET")
 			            .filters(f-> f.rewritePath("/lev4rec/","/lev4rec/"))
-			            .uri("http://localhost:8888"))
+			            .uri("http://spring-service:8888"))
 			        
 			        .route(p-> p.path("/lev4rec/").and().method("POST")
 				            .filters(f-> f.rewritePath("/lev4rec/","/lev4rec/"))
-				            .uri("http://localhost:8888"))
+				            .uri("http://spring-service:8888"))
 			        
 			        .route(p-> p.path("/lev4rec/dsl").and().method("GET")
 				            .filters(f-> f.rewritePath("/lev4rec/dsl","/lev4rec/dsl/"))
-				            .uri("http://localhost:8888"))
+				            .uri("http://spring-service:8888"))
 			        
 			        .route(p-> p.path("/lev4rec/dsl").and().method("POST")
 				            .filters(f-> f.rewritePath("/lev4rec/dsl/","/lev4rec/dsl/"))
-				            .uri("http://localhost:8888"))
+				            .uri("http://spring-service:8888"))
 			        
 			        .route(p-> p.path("/lev4rec/generate").and().method("GET")
 				            .filters(f-> f.rewritePath("/lev4rec/generate","/lev4rec/generate"))
-				            .uri("http://localhost:8888"))
+				            .uri("http://spring-service:8888"))
 			        
 			        
 			        .route(p-> p.path("/lev4rec/generate").and().method("POST")
 				            .filters(f-> f.rewritePath("/lev4rec/generate","/lev4rec/generate"))
-				            .uri("http://localhost:8888"))
+				            .uri("http://spring-service:8888"))
 			        
 			        
 			        .route(p->p.path("/lev4rec/webjars/requirejs/2.3.6/require.min.js")
 			        		.filters(f-> f.rewritePath("/lev4rec/dsl","/lev4rec/dsl"))
-			        		.uri("http://localhost:8888"))
+			        		.uri("http://spring-service:8888"))
 			        
 			        
 			        .route(p->p.path("/lev4rec/css/xtext-ace.css")
 			        		.filters(f-> f.rewritePath("/lev4rec/","/lev4rec/"))
-			        		.uri("http://localhost:8888"))
+			        		.uri("http://spring-service:8888"))
 			        
 			        .route(p->p.path("/lev4rec/css/style.css")
 			        		.filters(f-> f.rewritePath("/lev4rec/","/lev4rec/"))
-			        		.uri("http://localhost:8888"))	
+			        		.uri("http://spring-service:8888"))	
 			        
 			        .route(p->p.path("/lev4rec/css/formStyle.css")
 			        		.filters(f-> f.rewritePath("/lev4rec/","/lev4rec/"))
-			        		.uri("http://localhost:8888"))	
+			        		.uri("http://spring-service:8888"))	
 			        
 			        .route(p->p.path("/lev4rec/css/formStyle2.css")
 			        		.filters(f-> f.rewritePath("/lev4rec/","/lev4rec/"))
-			        		.uri("http://localhost:8888"))	   
+			        		.uri("http://spring-service:8888"))	   
 			        
 			        .route(p->p.path("/lev4rec/webjars/bootstrap/3.3.7-1/css/bootstrap.min.css/")
 			        		.filters(f-> f.rewritePath("/lev4rec/","/lev4rec/"))
-			        		.uri("http://localhost:8888"))
+			        		.uri("http://spring-service:8888"))
 			        
 			        
 			        .route(p->p.path("lev4rec/webjars/bootstrap/3.3.7-1/css/bootstrap.min.css/bootstrap.min.css.map/")
 			        		.filters(f-> f.rewritePath("/lev4rec/","/lev4rec/"))
-			        		.uri("http://localhost:8888"))
+			        		.uri("http://spring-service:8888"))
 			        
 			        .route(p->p.path("/lev4rec/webjars/bootstrap/3.3.7-1/js/bootstrap.min.js")
 			        		.filters(f-> f.rewritePath("/lev4rec/","/lev4rec/"))
-			        		.uri("http://localhost:8888"))
+			        		.uri("http://spring-service:8888"))
 			        
 			        
 			        .route(p->p.path("/lev4rec/js/xtext-ace.js")
 			        		.filters(f-> f.rewritePath("/lev4rec/","/lev4rec/"))
-			        		.uri("http://localhost:8888"))
+			        		.uri("http://spring-service:8888"))
 			        
 			        .route(p->p.path("/lev4rec/webjars/jquery/3.4.1/jquery.min.js")
 			        		.filters(f-> f.rewritePath("/lev4rec/","/lev4rec/"))
-			        		.uri("http://localhost:8888"))
+			        		.uri("http://spring-service:8888"))
 			        
 			        
 			        .route(p->p.path("/lev4rec/webjars/ace/1.3.3/src/ace.js")
 			        		.filters(f-> f.rewritePath("/lev4rec/dsl","/lev4rec/dsl"))
-			        		.uri("http://localhost:8888"))
+			        		.uri("http://spring-service:8888"))
 			        
 			        .route(p->p.path("/lev4rec/webjars/ace/1.3.3/src/ext-language_tools.js")
 			        		.filters(f-> f.rewritePath("/lev4rec/dsl","/lev4rec/dsl"))
-			        		.uri("http://localhost:8888"))
+			        		.uri("http://spring-service:8888"))
 			        
 			        .route(p->p.path("/lev4rec/webjars/ace/1.3.3/src/theme-eclipse.js")
 			        		.filters(f-> f.rewritePath("/lev4rec/dsl","/lev4rec/dsl"))
-			        		.uri("http://localhost:8888"))
+			        		.uri("http://spring-service:8888"))
 			        
 			        .route(p->p.path("/lev4rec/js/mode-rec.js")
 			        		.filters(f-> f.rewritePath("/lev4rec/","/lev4rec/"))
-			        		.uri("http://localhost:8888"))
+			        		.uri("http://spring-service:8888"))
 			        
 			        .route(p->p.path("/xtext-service/update").and().method("PUT")
 			        		.filters(f-> f.rewritePath("/xtext-service/update","/xtext-service/update"))
-			        		.uri("http://localhost:8080"))
+			        		.uri("http://xtext-service:8080"))
 			        
 			        .route(p->p.path("/xtext-service/validate").and().method("GET")
 			        		.filters(f-> f.rewritePath("/xtext-service/validate","/xtext-service/validate"))
-			        		.uri("http://localhost:8080"))
+			        		.uri("http://xtext-service:8080"))
 			        
 			        
 			        .route(p->p.path("/xtext-service/assist").and().method("GET")
 			        		.filters(f-> f.rewritePath("/xtext-service/assist","/xtext-service/assist"))
-			        		.uri("http://localhost:8080"))
+			        		.uri("http://xtext-service:8080"))
 			        
 			        .route(p->p.path("/xtext-service/occurrences").and().method("GET")
 			        		.filters(f-> f.rewritePath("/xtext-service/occurrences","/xtext-service/occurrences"))
-			        		.uri("http://localhost:8080"))
+			        		.uri("http://xtext-service:8080"))
 			        
 			        .route(p->p.path("/xtext-service/assist").and().method("GET")
 			        		.filters(f-> f.rewritePath("/xtext-service/assist","/xtext-service/assist"))
-			        		.uri("http://localhost:8080"))
+			        		.uri("http://xtext-service:8080"))
 			        
 			        .route(p->p.path("/xtext-service/assist").and().method("POST")
 			        		.filters(f-> f.rewritePath("/xtext-service/assist","/xtext-service/assist"))
-			        		.uri("http://localhost:8080"))
+			        		.uri("http://xtext-service:8080"))
 			        
 			        .route(p->p.path("/xtext-service/format").and().method("POST")
 			        		.filters(f-> f.rewritePath("/xtext-service/format","/xtext-service/format"))
-			        		.uri("http://localhost:8080"))
+			        		.uri("http://xtext-service:8080"))
 			        
 			        
 			        .build();
